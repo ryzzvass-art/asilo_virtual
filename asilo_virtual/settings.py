d9484ce00969962e14e7785a1cdc48dee9d8d6ec
ryzzvass-art <ryzzvass@gmail.com>
2026-04-16 21:29:45 -0400

    T-04: REST Framework configurado, URL base de API creada

--- asilo_virtual/settings.py
+++ asilo_virtual/settings.py
@@ -132,3 +132,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+# Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+}
