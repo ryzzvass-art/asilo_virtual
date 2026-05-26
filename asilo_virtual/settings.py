e110413d7f69f66b4428c28900d4dc3d18372140
ryzzvass-art <ryzzvass@gmail.com>
2026-05-26 12:27:11 -0400

    modificacion de puerto

--- asilo_virtual/settings.py
+++ asilo_virtual/settings.py
@@ -49,9 +49,9 @@ else:
     # Desarrollo local (Gmail SMTP)
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_USE_SSL = False
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
     EMAIL_HOST_USER = config('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
